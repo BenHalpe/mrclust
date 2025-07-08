18753026c47d6fc7a7b782a04317396a1d9efac2
BenHalpe <63581109+BenHalpe@users.noreply.github.com>
2025-07-08 13:37:14 +0300

    Update log_norm.R

--- R/log_norm.R
+++ R/log_norm.R
@@ -1,8 +1,8 @@
-log_norm <- function(i, j, tmp_lgt, tmp_lgt2, theta, theta_sd, theta_clust) {
+log_norm <- function(i, j, tmp_lgt, tmp_lgt2, theta, theta_sd, theta_clust) { 
   tmp_theta <- rep(theta[i], tmp_lgt2)
-  tmp_theta_sd <- rep(theta_sd[i], tmp_lgt2)
-  tmp_clust <- rep(theta_clust[j], each = tmp_lgt)
+  tmp_theta_sd <- rep(theta_sd[i], tmp_lgt2) # replication for vectorization, as we calculate using theta each time across all cluster centers
+  tmp_clust <- rep(theta_clust[j], each = tmp_lgt) 
   res <- (-0.5 * log(2 * pi) - log(tmp_theta_sd)
-          - 0.5 * (tmp_theta - tmp_clust)^2 / tmp_theta_sd^2)
+          - 0.5 * (tmp_theta - tmp_clust)^2 / tmp_theta_sd^2)  # just the log of the gaussian pdf
   return(res)
 }
